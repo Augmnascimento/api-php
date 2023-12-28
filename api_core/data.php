e68ce521e9acb3c2c7866aa1649c930c956cb6d5
Augusto <augmnascimento@gmail.com>
2023-12-28 15:55:24 -0300

    Api em php puro

---
+++ api_core/data.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+    ['name'=>'carlos', 'email'=>'carlos@yahoo.com'],
+    ['name'=>'marinete', 'email'=>'marinete@microsoft.com'],
+    ['name'=>'ronaldo', 'email'=>'ronaldo@gmail.com'],
+    ['name'=>'marcio', 'email'=>'marcio@yahoo.com'],
+    ['name'=>'jubileu', 'email'=>'jubileu@hotmail.com'],
+    ['name'=>'miguel', 'email'=>'miguel@gmail.com'],
+    ['name'=>'juliana', 'email'=>'juliana@apple.com'],
+    ['name'=>'jason', 'email'=>'jason@hotmail.com'],
+];
